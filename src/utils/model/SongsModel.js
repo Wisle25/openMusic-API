86de6f297fbe7c0223cb00a39cd9ffce5188444e
Wisle25 <handidwic12@gmail.com>
2021-09-03 09:49:58 +0700

    changed some codes on Song Models

--- src/utils/model/SongsModel.js
+++ src/utils/model/SongsModel.js
@@ -1,20 +1,6 @@
 
-const getSongByIdModel = ({
-    id,
-    title,
-    year,
-    performer,
-    genre,
-    duration,
-    inserted_at,
-    updated_at
-}) => ({
-    id,
-    title,
-    year,
-    performer,
-    genre,
-    duration,
+const getSongByIdModel = ({ inserted_at, updated_at, ...args }) => ({
+    ...args,
     insertedAt: inserted_at,
     updatedAt: updated_at
 })
